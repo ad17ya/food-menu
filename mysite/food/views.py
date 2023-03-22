829db718f6ac103729ea2e985e4f55bbbb071121
Aditya Kshirsagar <kaditya.anil@gmail.com>
2023-03-22 22:53:23 +0530

    update functionality added

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -30,3 +30,13 @@ def create_item(request):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', {'form': form})
+
+def update_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html', {'form': form, 'item': item})
